53be738cb9d74b2661749b08944582bc31a2a89a
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2022-05-02 17:36:24 -0400

    Add comments to ExampleProgram.h

--- ExampleProgram.h
+++ ExampleProgram.h
@@ -4,6 +4,17 @@
 
 #include "Program.h"
 
+/*
+ * StartProgram
+ * AUTHOR: Logan Pfahler
+ * DATE: 5/1/2022
+ *
+ * PARAMS
+ * ButtonLamp *l    -> lamp pointer
+ *
+ * DESCRIPTION
+ * Fades in from off to white. A good program to be the first in a program controller.
+ */
 class StartProgram : public ButtonProgram
 {
 public:
@@ -22,6 +33,18 @@ public:
     void loop() {}
 };
 
+/*
+ * ColorProgram
+ * AUTHOR: Logan Pfahler
+ * DATE: 5/1/2022
+ *
+ * PARAMS
+ * ButtonLamp *l    -> lamp pointer
+ * CRGB c           -> color to display on the lamp
+ *
+ * DESCRIPTION
+ * Sets the lamp to a solid color c.
+ */
 class ColorProgram : public ButtonProgram
 {
 private:
@@ -43,6 +66,17 @@ public:
     void loop() {}
 };
 
+/*
+ * RainbowProgram
+ * AUTHOR: Logan Pfahler
+ * DATE: 5/1/2022
+ *
+ * PARAMS
+ * ButtonLamp *l    -> lamp pointer
+ *
+ * DESCRIPTION
+ * Moves a gradient of colors up the lamp, giving a fading up effect.
+ */
 class RainbowProgram : public ButtonProgram
 {
 private:
@@ -64,6 +98,84 @@ public:
         lamp->Delay(100);
         i++;
         i %= 255;
+    }
+};
+
+/*
+ * BouncingBallProgram
+ * AUTHOR: Logan Pfahler
+ * DATE: 5/1/2022
+ *
+ * PARAMS
+ * ButtonLamp *l    -> lamp pointer
+ *
+ * DESCRIPTION
+ * Bounces a ball at a random starting veloctiy. Repeats once the ball gets below a certain velocity
+ */
+class BouncingBallProgram : public ButtonProgram
+{
+
+public:
+    BouncingBallProgram(ButtonLamp *l) : ButtonProgram(l) {}
+
+    float GAP = 0.015;
+    float pos = 0;
+
+    float velocity;
+    float gravity;
+    float del;
+    float cons_energy;
+    float vmin;
+    float vmax;
+
+    void setup()
+    {
+        vmin = 2;
+        vmax = 7;
+        gravity = 9.81;
+        del = 10;
+        cons_energy = 0.8;
+    }
+
+    void loop()
+    {
+        if (pos == 0 && abs(velocity) < 0.75)
+            velocity = randomVelocity();
+
+        lamp->SetLamp(CRGB(100, 100, 100));
+        draw();
+        bounce();
+        lamp->Delay(del);
+    }
+
+    // Updates the positon and velocity of the ball for a change of del seconds
+    void bounce()
+    {
+        pos += (velocity * del / 1000) / GAP;
+        velocity -= gravity * del / 1000;
+
+        if (pos < 0)
+        {
+            velocity = -1 * velocity * cons_energy;
+            pos = 0;
+        }
+    }
+
+    // Displays the ball on the lamp
+    void draw()
+    {
+        float p_d = pos - floor(pos);
+        lamp->SetRing(pos - 1, CRGB(255 * (1 - p_d), 0, 0));
+        lamp->SetRings(pos, pos + 2, CRGB(255, 0, 0));
+        lamp->SetRing(pos + 2, CRGB(255 * (p_d), 0, 0));
+        lamp->Show();
+    }
+
+    // Picks a random velocity from vmin to vmax
+    float randomVelocity()
+    {
+        float v = (rand() % int((vmax - vmin) * 10) + (vmin * 10)) / 10.0;
+        return v;
     }
 };
 
